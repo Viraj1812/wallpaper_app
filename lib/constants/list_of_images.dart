2d91f04d1d45bc2df936224dc4fa8d125a1be905
Viraj1812 <viraj.vasani1218@gmail.com>
2023-12-27 14:38:41 +0530

    small ui changes

--- lib/constants/list_of_images.dart
+++ lib/constants/list_of_images.dart
@@ -1,19 +1,25 @@
 const List<String> nature = [
-  'https://images.pexels.com/photos/719396/pexels-photo-719396.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.pexels.com/photos/1684151/pexels-photo-1684151.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'https://images.pexels.com/photos/1271620/pexels-photo-1271620.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'https://images.pexels.com/photos/1047051/pexels-photo-1047051.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'https://images.pexels.com/photos/1146127/pexels-photo-1146127.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'https://images.pexels.com/photos/19499799/pexels-photo-19499799/free-photo-of-waterfall-over-rocks.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'https://images.pexels.com/photos/19500080/pexels-photo-19500080/free-photo-of-people-on-trail-by-waterfall.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
 ];
 
 const List<String> cars = [
-  'https://images.pexels.com/photos/1535162/pexels-photo-1535162.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.pexels.com/photos/1707215/pexels-photo-1707215.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.pexels.com/photos/719396/pexels-photo-719396.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.pexels.com/photos/1684151/pexels-photo-1684151.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'https://images.pexels.com/photos/14754436/pexels-photo-14754436.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'https://images.pexels.com/photos/19021285/pexels-photo-19021285/free-photo-of-vintage-cars-in-heydar-aliyev-center-in-baku.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'https://images.pexels.com/photos/13971806/pexels-photo-13971806.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'https://images.pexels.com/photos/11652118/pexels-photo-11652118.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'https://images.pexels.com/photos/12644285/pexels-photo-12644285.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
 ];
 
 const List<String> animals = [
-  'https://images.pexels.com/photos/799443/pexels-photo-799443.jpeg',
-  'https://images.pexels.com/photos/1624496/pexels-photo-1624496.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  'https://images.pexels.com/photos/2486168/pexels-photo-2486168.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  'https://images.pexels.com/photos/19535284/pexels-photo-19535284/free-photo-of-horses-and-colts-with-cowboy-with-whip-riding-behind.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'https://images.pexels.com/photos/16278340/pexels-photo-16278340/free-photo-of-cats-in-box.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'https://images.pexels.com/photos/16252676/pexels-photo-16252676/free-photo-of-dogs-playing-among-autumn-leaves.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'https://images.pexels.com/photos/6321866/pexels-photo-6321866.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  'https://images.pexels.com/photos/14659604/pexels-photo-14659604.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
 ];
 
 const List<String> category = ['Nature', 'Cars', 'Animals'];
--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -15,69 +15,71 @@ class _HomeScreenState extends State<HomeScreen> {
   String cat = "Nature";
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Text(
-            'Wallpaper App',
-            style: GoogleFonts.montserrat(
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-              fontSize: 20,
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(
+          'Wallpaper App',
+          style: GoogleFonts.montserrat(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+        ),
+        backgroundColor: const Color.fromARGB(255, 236, 236, 236),
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(16),
+            height: 100,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: category.length,
+              itemBuilder: (context, index) {
+                return CategoryCard(
+                    categoryName: category[index],
+                    isSelected: cat == category[index],
+                    onTap: () {
+                      setState(() {
+                        cat = category[index];
+                      });
+                    });
+              },
             ),
           ),
-          backgroundColor: Colors.white,
-        ),
-        body: Column(
-          children: [
-            Container(
+          const Divider(
+            height: 0.5,
+            color: Colors.grey,
+          ),
+          Expanded(
+            child: Container(
               padding: const EdgeInsets.all(16),
-              height: 100,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: category.length,
-                itemBuilder: (context, index) {
-                  return CategoryCard(
-                      categoryName: category[index],
-                      isSelected: cat == category[index],
-                      onTap: () {
-                        setState(() {
-                          cat = category[index];
-                        });
-                      });
-                },
-              ),
-            ),
-            Expanded(
-              child: Container(
-                padding: const EdgeInsets.all(16),
-                child: GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    crossAxisSpacing: 8,
-                    mainAxisSpacing: 8,
-                    childAspectRatio: 0.5,
-                  ),
-                  itemCount: wallpapers[cat]!.length,
-                  itemBuilder: (context, index) => InkWell(
-                    onTap: () => Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => FullImage(
-                            imageUrl: wallpapers[cat]![index],
-                          ),
-                        )),
-                    child: Hero(
-                      tag: wallpapers[cat]![index],
-                      child: Container(
-                        color: Colors.white30,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(20),
-                          child: Image.network(
-                            wallpapers[cat]![index],
-                            fit: BoxFit.cover,
-                          ),
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 8,
+                  mainAxisSpacing: 8,
+                  childAspectRatio: 0.5,
+                ),
+                itemCount: wallpapers[cat]!.length,
+                itemBuilder: (context, index) => InkWell(
+                  onTap: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => FullImage(
+                          imageUrl: wallpapers[cat]![index],
+                        ),
+                      )),
+                  child: Hero(
+                    tag: wallpapers[cat]![index],
+                    child: Container(
+                      color: Colors.white30,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Image.network(
+                          wallpapers[cat]![index],
+                          fit: BoxFit.cover,
                         ),
                       ),
                     ),
@@ -85,8 +87,8 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
